a7088f537f1ee95d802767528a65b0b908814d0c
Stefan-23 <sdrmanic@gmail.com>
2020-06-02 20:10:23 +0200

    Addin UI, User Auth(register & login)

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,10 @@
     //Load Config
     require_once 'config/config.php';
 
+    //Load helpers
+    //Helpers
+    require_once 'helpers/url_helper.php';
+    require_once 'helpers/session_helper.php';
 
 
     //Autoload libraries
---
+++ app/views/Users/register.php
@@ -0,0 +1,48 @@
+<?php require APPROOT . '/views/inc/header.php';?>
+
+    <div class="row">
+        <div class="col-md-6 mx-auto">
+            <div class="card card-body bg-light mt-5">
+                <h2>Create An Account</h2>
+                <p> Please fill form </p>
+                <form action="<?php echo URLROOT; ?> users/register" method="POST">
+                    
+                    <div class="form-group">
+                        <label for="name"> Name: <sup>*</sup> </label>
+                            <input type="text" name="name" class="form-control form control-lg <?php echo (!empty($data['err_name'])) ? 'is-invalid' : '' ; ?>" value = "<?php echo $data['name']; ?>"> 
+                            <span class="invalid-feedback"> <?php echo $data['err_name']; ?> </span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email"> E-mail: <sup>*</sup> </label>
+                            <input type="email" name="email" class="form-control form control-lg <?php echo (!empty($data['err_email'])) ? 'is-invalid' : '' ; ?>" value = "<?php echo $data['email']; ?>"> 
+                            <span class="invalid-feedback"> <?php echo $data['err_email']; ?> </span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password"> Password: <sup>*</sup> </label>
+                            <input type="password" name="password" class="form-control form control-lg <?php echo (!empty($data['err_password'])) ? 'is-invalid' : '' ; ?>" value = "<?php echo $data['password']; ?>"> 
+                            <span class="invalid-feedback"> <?php echo $data['err_password']; ?> </span>
+                    </div> 
+
+                    <div class="form-group">
+                        <label for="confirm_password"> Confirm-password: <sup>*</sup> </label>
+                            <input type="password" name="confirm_password" class="form-control form control-lg <?php echo (!empty($data['err_confirm_password'])) ? 'is-invalid' : '' ; ?>" value = "<?php echo $data['confirm_password']; ?>"> 
+                            <span class="invalid-feedback"> <?php echo $data['err_confirm_password']; ?> </span>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col">
+                            <input type="submit" value="Register" class= "btn btn-success btn-block">
+                        </div>
+                        <div class="col">
+                            <a href="<?php echo URLROOT ?>users/login" class= "btn btn-light btn-block">Already member? Login then. </a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+<?php require APPROOT . '/views/inc/footer.php';?>
